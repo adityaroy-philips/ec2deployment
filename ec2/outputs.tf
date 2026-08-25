7a537ccff4d7c76daaf577d80dc718c416034e77
adityaroy-philips <aditya.roy@philips.com>
2026-08-25 15:07:48 +0530

    Add outputs for EC2 instance and VPC IDs

---
+++ ec2/outputs.tf
@@ -0,0 +1,10 @@
+hcl
+output "instance_id" {
+  description = "The ID of the Windows EC2 instance"
+  value       = aws_instance.windows_server.id
+}
+
+output "vpc_id" {
+  description = "The ID of the custom VPC"
+  value       = aws_vpc.main.id
+}

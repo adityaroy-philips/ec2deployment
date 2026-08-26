e37e4a63a4f0025c18ff8f7588912605bbcf0d35
adityaroy-philips <aditya.roy@philips.com>
2026-08-26 11:15:43 +0530

    Rename instance_profile to iam_instance_profile

--- ec2/main.tf
+++ ec2/main.tf
@@ -125,7 +125,7 @@ resource "aws_instance" "windows_server" {
   instance_type        = "t3.medium" # Minimum recommended sizing for Windows Server
   subnet_id            = aws_subnet.private.id
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
-  instance_profile     = aws_iam_instance_profile.ec2_profile.name
+  iam_instance_profile     = aws_iam_instance_profile.ec2_profile.name
 
   metadata_options {
     http_endpoint = "enabled"
